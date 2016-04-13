5795dcc5aa9917e490cc5177353376f7a384e016
reinbauwens <rein.bauwens@student.odisee.be>
2016-04-13 22:13:50 +0200

    updath path

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,9 @@
 // Require Composer Autoloader
 require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+//set date time zone
+date_default_timezone_set("America/Managua");
+
 // Create new Silex App
 $app = new Silex\Application();
 
