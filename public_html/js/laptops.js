b55369d99e568d5840073d5cbed4b80b7c284602
reinbauwens <rein.bauwens@student.odisee.be>
2016-05-03 20:57:31 +0200

    fixes

--- public_html/js/laptops.js
+++ public_html/js/laptops.js
@@ -340,7 +340,7 @@ $( "#AddLaptop" ).click(function() {
                   cell10.innerHTML = Model;
                   cell11.innerHTML = Status;
                   cell12.innerHTML = Uuid;
-                  cell12.addClass('uuid displaynone');
+                  cell12.setAttribute("class", 'uuid displaynone');
                   cell13.innerHTML = ((assignee == People)?'yes':'no');
 
                   //if succes get the id of the newest added laptop to make the buttons work
